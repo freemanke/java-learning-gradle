cbadd699aeec122fa94b9bada94e51136e67a3d9
Freeman Ke <freemanke@qq.com>
2017-09-11 14:46:00 +0800

    init

---
+++ src/main/java/com/freemanke/javalearninggradle/JavaLearningGradleApplication.java
@@ -0,0 +1,12 @@
+package com.freemanke.javalearninggradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearningGradleApplication.class, args);
+	}
+}
